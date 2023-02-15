515d6071e19529939e3ea0df6f354ece8b06f936
Ismail Mohamad <ikmohamad19@gmail.com>
2023-02-15 17:21:46 +0000

    test: add tests for missing fields

--- src/handlers/user/createUser.test.js
+++ src/handlers/user/createUser.test.js
@@ -1,21 +1,91 @@
 const createUser = require("./createUser")
 const eventGenerator = require("../../tests/utils/eventGenerator")
+const validators = require("../../tests/utils/validators")
 
 const context = {
   callbackWaitsForEmptyEventLoop: true,
 }
 
 describe("POST /user", () => {
-  test("Should return 400 if required fields are null", async () => {
-    const event = eventGenerator({
-      body: {},
+  describe("Return 400 if missing required fields", () => {
+    test("Should return 400 if all required fields are undefined", async () => {
+      const event = eventGenerator({
+        body: {},
+      })
+
+      const res = await createUser.handler(event, context)
+
+      expect(validators.isApiGatewayResponse(res)).toBe(true)
+      expect(res.statusCode).toBe(400)
+      expect(JSON.parse(res.body).message).toBe("All fields are required")
     })
-    const res = await createUser.handler(event, context)
-    expect(res.statusCode).toBe(400)
-    expect(JSON.parse(res.body).message).toBe("All fields are required")
+
+    test("Should return 400 if username is evaluates to false", async () => {
+      const event = eventGenerator({
+        body: {
+          username: "",
+          name: "name",
+          password: "password",
+          email: "email",
+        },
+      })
+
+      const res = await createUser.handler(event, context)
+
+      expect(validators.isApiGatewayResponse(res)).toBe(true)
+      expect(res.statusCode).toBe(400)
+      expect(JSON.parse(res.body).message).toBe("All fields are required")
+    })
+
+    test("Should return 400 if name is evaluates to false", async () => {
+      const event = eventGenerator({
+        body: {
+          username: "username",
+          name: "",
+          password: "password",
+          email: "email",
+        },
+      })
+
+      const res = await createUser.handler(event, context)
+
+      expect(validators.isApiGatewayResponse(res)).toBe(true)
+      expect(res.statusCode).toBe(400)
+      expect(JSON.parse(res.body).message).toBe("All fields are required")
+    })
+
+    test("Should return 400 if password is evaluates to false", async () => {
+      const event = eventGenerator({
+        body: {
+          username: "username",
+          name: "name",
+          password: "",
+          email: "email",
+        },
+      })
+
+      const res = await createUser.handler(event, context)
+
+      expect(validators.isApiGatewayResponse(res)).toBe(true)
+      expect(res.statusCode).toBe(400)
+      expect(JSON.parse(res.body).message).toBe("All fields are required")
+    })
+
+    test("Should return 400 if email is evaluates to false", async () => {
+      const event = eventGenerator({
+        body: {
+          username: "username",
+          name: "name",
+          password: "password",
+          email: "",
+        },
+      })
+
+      const res = await createUser.handler(event, context)
+
+      expect(validators.isApiGatewayResponse(res)).toBe(true)
+      expect(res.statusCode).toBe(400)
+      expect(JSON.parse(res.body).message).toBe("All fields are required")
+    })
   })
 })
-
-// https://github.com/AppGambitStudio/serverless-testing
-// https://github.com/chief-wizard/serverless-jest-example
-// https://mongoosejs.com/docs/lambda.html
