534149f4302bdb4f769619d9c293f7b42f514b2c
Ismail Mohamad <ikmohamad19@gmail.com>
2023-02-15 13:00:50 +0000

    chore: skip db connection if testing

--- src/database/dbConn.js
+++ src/database/dbConn.js
@@ -4,7 +4,12 @@ let conn = null
 
 const uri = process.env.DATABASE_URI
 
+mongoose.set("strictQuery", false)
+
 module.exports = connectDatabase = async () => {
+  if (process.env.NODE_ENV === "test") {
+    return
+  }
   if (conn == null) {
     // create new connection if no connection exists
     conn = await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 })
