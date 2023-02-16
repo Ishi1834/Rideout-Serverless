13f43768617f6519c766f3a6c8c85e31a4913f56
Ismail Mohamad <ikmohamad19@gmail.com>
2023-02-16 19:02:29 +0000

    chore: test checkUsernameEmailIsDuplicate method

--- src/tests/setUpFile.js
+++ src/tests/setUpFile.js
@@ -6,6 +6,10 @@ beforeAll(async () => {
   await mongoose.connect(process.env["MONGO_URI"])
 })
 
+afterEach(async () => {
+  await mongoose.connection.db.dropDatabase()
+})
+
 afterAll(async () => {
   await mongoose.disconnect()
 })
---
+++ src/utils/database/users.test.js
@@ -0,0 +1,44 @@
+const { checkUsernameEmailIsTaken, saveUser } = require("./users")
+const User = require("../../models/User")
+
+const testUser = {
+  username: "username",
+  name: "name",
+  password: "password",
+  email: "email",
+}
+
+describe("All function that interact with DB work correctly", () => {
+  describe("CheckUsernameEmailIsTaken works correctly", () => {
+    test("Returns 'email' if email is duplicate", async () => {
+      await User.create(testUser)
+
+      const duplicate = await checkUsernameEmailIsTaken({
+        username: "not duplicate username",
+        email: "email",
+      })
+
+      expect(duplicate).toBe("email")
+    })
+
+    test("Returns 'username' if username is duplicate", async () => {
+      await User.create(testUser)
+
+      const duplicate = await checkUsernameEmailIsTaken({
+        username: "username",
+        email: "not duplicate email",
+      })
+
+      expect(duplicate).toBe("username")
+    })
+  })
+
+  test("Returns null if username and email are not duplicates", async () => {
+    const duplicate = await checkUsernameEmailIsTaken({
+      username: "username",
+      email: "email",
+    })
+
+    expect(duplicate).toBe(null)
+  })
+})
