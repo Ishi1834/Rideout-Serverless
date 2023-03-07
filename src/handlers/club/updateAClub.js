4ff9c16a2dab3070d26f1a51fffffd3710f0d4f2
Ismail Mohamad <ikmohamad19@gmail.com>
2023-03-07 14:43:55 +0000

    chore: add update club function

---
+++ src/handlers/club/updateAClub.js
@@ -0,0 +1,61 @@
+const connectDatabase = require("../../config/dbConn")
+const { DBFindClubById } = require("../../utils/database/clubs")
+const Responses = require("../../utils/apiResponses")
+const logger = require("../../utils/logger")
+
+module.exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+
+  try {
+    await connectDatabase()
+    const { clubId } = event.pathParameters
+    const { location, tags, description, city } = JSON.parse(event.body)
+
+    if (!location && !tags && !description && !city) {
+      return Responses._400({
+        message: "Atleast 1 property must be given, to update club",
+      })
+    }
+    if (location && (!Array.isArray(location) || location.length !== 2)) {
+      return Responses._400({
+        message: "Location should be an array of 2 numbers",
+      })
+    }
+    if (tags && !Array.isArray(tags)) {
+      return Responses._400({
+        message: "Tags should be an array",
+      })
+    }
+
+    const club = await DBFindClubById(clubId)
+    if (!club) {
+      return Responses._400({ message: "Invalid club" })
+    }
+
+    if (location) {
+      club.location = {
+        type: "Point",
+        coordinates: location,
+      }
+    }
+    if (city) {
+      club.city = city
+    }
+    if (description) {
+      club.description = description
+    }
+    if (tags) {
+      club.tags = tags
+    }
+
+    const updatedClub = await club.save()
+
+    return Responses._200({
+      message: "Club updated",
+      club: updatedClub,
+    })
+  } catch (error) {
+    logger(error)
+    return Responses._500({ error: error?.message })
+  }
+}
