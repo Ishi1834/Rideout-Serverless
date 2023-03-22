11aba757d6d0ba68ed804a0c969a1b8c11ef5b9a
Ismail Mohamad <ikmohamad19@gmail.com>
2023-03-22 18:09:08 +0000

    test: integration test for joinAClub

---
+++ tests/integration/clubs/joinAClub.integration.test.js
@@ -0,0 +1,37 @@
+const axios = require("axios")
+const dbHelper = require("../../utils/dbHelper")
+
+describe("PATCH /clubs/{clubId}/join integration test", () => {
+  test("User can request to join a club", async () => {
+    const clubObject = {
+      name: "clubName",
+      location: {
+        type: "Point",
+        coordinates: [50, 50],
+      },
+      city: "London",
+    }
+    const { _id: clubId } = await dbHelper.addClubToDB(clubObject)
+    const userObject = {
+      username: "username",
+      email: "user@email.com",
+      password: "password",
+      name: "user",
+    }
+    const { authToken } = await dbHelper.getValidUserTokens(userObject)
+
+    const res = await axios.patch(
+      `http://localhost:3000/clubs/${clubId}/join`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        validateStatus: () => true,
+      }
+    )
+
+    expect(res.status).toBe(200)
+    expect(res.data.message).toBe("Join request sent")
+  })
+})
