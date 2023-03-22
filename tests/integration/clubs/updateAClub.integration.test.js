793a0ac2c75237f0344d6b3bbcbf18826016d3e3
Ismail Mohamad <ikmohamad19@gmail.com>
2023-03-22 17:49:17 +0000

    test: integration test updateAClub

---
+++ tests/integration/clubs/updateAClub.integration.test.js
@@ -0,0 +1,45 @@
+const axios = require("axios")
+const dbHelper = require("../../utils/dbHelper")
+
+describe("Update /clubs/{clubId} integration test", () => {
+  test("User can update a club if they have role 'admin'", async () => {
+    const userObject = {
+      username: "username",
+      email: "user@email.com",
+      password: "password",
+      name: "user",
+    }
+    const clubObject = {
+      name: "clubName",
+      location: {
+        type: "Point",
+        coordinates: [50, 50],
+      },
+      city: "London",
+    }
+    const { authToken, clubId } = await dbHelper.getValidUserTokenWithClub(
+      userObject,
+      clubObject
+    )
+
+    const body = {
+      location: [20, 30],
+      city: "Manchester",
+      tags: ["Road Cycling"],
+    }
+
+    const res = await axios.patch(
+      `http://localhost:3000/clubs/${clubId}`,
+      body,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        validateStatus: () => true,
+      }
+    )
+
+    expect(res.status).toBe(200)
+    expect(res.data.message).toBe("Club updated")
+  })
+})
