cd3a13fe0fd02d45d31ca15559eb7490101afd6f
Ismail Mohamad <ikmohamad19@gmail.com>
2023-03-24 10:33:53 +0000

    test: leaveAClubRide integration test

---
+++ tests/integration/rides/leaveAClubRide.integration.test.js
@@ -0,0 +1,83 @@
+const axios = require("axios")
+const dbHelper = require("../../utils/dbHelper")
+const mongoose = require("mongoose")
+
+describe("PATCH /clubs/{clubId}/rides/{rideId}/leave integration test", () => {
+  test("User can leave a club ride if they have joined", async () => {
+    const userId = mongoose.Types.ObjectId()
+    const clubId = mongoose.Types.ObjectId()
+    const rideCreatorId = mongoose.Types.ObjectId()
+    const rideId = mongoose.Types.ObjectId()
+    const date = new Date().getTime() + 10000
+
+    await dbHelper.addClubToDB({
+      _id: clubId,
+      name: "clubname",
+      location: {
+        type: "Point",
+        coordinates: [50, 50],
+      },
+      city: "London",
+      rides: [rideId],
+      members: [
+        {
+          username: "username",
+          userId: userId,
+          authorization: "user",
+        },
+      ],
+    })
+    await dbHelper.addRideToDB({
+      _id: rideId,
+      name: "ridename",
+      createdBy: {
+        username: "test11",
+        userId: rideCreatorId,
+      },
+      date: date,
+      clubId: clubId,
+      startLocation: {
+        type: "Point",
+        coordinates: [40, 60],
+      },
+      signedUpCyclists: [
+        {
+          username: "username",
+          userId: userId,
+        },
+      ],
+      description: "description",
+      rideType: "Social",
+      distance: 40,
+      speed: 25,
+    })
+    const { authToken } = await dbHelper.getValidUserTokens({
+      _id: userId,
+      username: "username",
+      email: "user@email.com",
+      password: "password",
+      name: "user",
+      rides: [rideId],
+      clubs: [
+        {
+          authorization: "user",
+          clubId: clubId,
+        },
+      ],
+    })
+
+    const res = await axios.patch(
+      `http://localhost:3000/clubs/${clubId}/rides/${rideId}/leave`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        validateStatus: () => true,
+      }
+    )
+
+    expect(res.status).toBe(200)
+    expect(res.data.message).toBe("You have left this ride")
+  })
+})
